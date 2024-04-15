9123734cddbfe29bc7a7cb3c99c979d0a6ffcbb1
senopaul <senopaul@gmail.com>
2024-04-15 10:40:37 +0300

    Fouth Commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class MyBlog(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    tag = models.CharField(max_length=50)
+    image =models.TextField()
+    views = models.IntegerField(default=7)
+    
+    
+    
+    #Commands
+    # -python manage.py makemigrations | blog ...(if there are many models in the project, add a name after make migrations)
+    # -python manage.py migrate ...send the models to the database
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import MyBlog
 # Create your views here.
+
+
+
+
+#create a new blog
+def blog(request):
+    blog_all = MyBlog.objects.all()
+    
 
 def blogView(request):
     template = loader.get_template('blog.html')
     return HttpResponse(template.render())
+
+
+#using the shell manage.py shell
