d119541908c0c12462c1e1d86d83a044ea14e0f6
senopaul <senopaul@gmail.com>
2024-04-16 08:05:37 +0300

    5th Commit

--- ContactUs/models.py
+++ ContactUs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models import
 
 # Create your models here.
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=50 ,null=False,blank=False,default='0')
+    email = models.EmailField(max_length=50 ,null=False)
+    phone = models.CharField(max_length=10)
+    content = models.TextField(max_length=)
